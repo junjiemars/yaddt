8ab0d1d218de450acbb3b92dd5e618113d1ca4be
南山竹 <junjiemars@gmail.com>
2021-06-11 14:32:49 +0800

    run

--- boot.js
+++ boot.js
@@ -9,23 +9,38 @@ const argv = process.argv.slice(process.argv.indexOf('--') !== -1
 const getopt = require('minimist');
 const options = getopt(argv);
 
+const help = [
+  "usage: yaddt.sh [options and files]",
+  "options:",
+  "  -h, --help                              display this help",
+  "  --what=[wechat,alipay]                  default is wechat dev tool",
+  "  --install                               install mini program dev tool",
+  "  --run                                   run mini programm dev tool",
+  "  --version                               print version and exit",
+].join("\n");
+
 if ((options._ && options.length > 0)
     || (options.h || options.help)) {
-  const help = [
-    "usage: yaddt.sh [options and files]",
-    "options:",
-    "  -h, --help                              display this help",
-    "  --install                               install mini program dev tool",
-    "  --version                               print version and exit",
-  ].join("\n");
   console.log(help);
   process.exit(1);
+}
+
+if (options.what) {
+  console.log(options.what);
 }
 
 if (options.install) {
   console.log('!TODO: install command');
   process.exit(0);
 }
+
+if (options.run) {
+  console.log('!TODO: run command');
+  process.exit(0);
+}
+
+console.log(help);
+process.exit(1);
 
 
 

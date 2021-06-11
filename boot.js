2a2ce24ba2733577048a1987a2725fbf52806460
南山竹 <junjiemars@gmail.com>
2021-06-11 14:13:46 +0800

    getopt

--- boot.js
+++ boot.js
@@ -4,19 +4,29 @@
 
 const process = require('process');
 const argv = process.argv.slice(process.argv.indexOf('--') !== -1
-																? process.argv.indexOf('--') + 1
-																: 1);
+                                ? process.argv.indexOf('--') + 1
+                                : 1);
 const getopt = require('minimist');
 const options = getopt(argv);
 
 if ((options._ && options.length > 0)
-		|| (options.h || options.help)) {
-		console.log('!TODO: show help');
-		process.exit(1);
+    || (options.h || options.help)) {
+  const help = [
+    "usage: yaddt.sh [options and files]",
+    "options:",
+    "  -h, --help                              display this help",
+    "  --install                               install mini program dev tool",
+    "  --version                               print version and exit",
+  ].join("\n");
+  console.log(help);
+  process.exit(1);
 }
 
 if (options.install) {
-		console.log('!TODO: install command');
+  console.log('!TODO: install command');
+  process.exit(0);
 }
+
+
 
 /* eof */
---
+++ yaddt.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+NODE="${NODE:-`command -v node`}"
+if [ ! -x "$NODE" ]; then
+  if [ ! -f "$HOME/.nvm/nvm.sh" ]; then
+    echo "!panic, node no found."
+    exit 1
+  fi
+  . "$HOME/.nvm/nvm.sh"
+  if [ 0 -ne `nvm --version 2>&1 >/dev/null; echo $?` ]; then
+    echo "!panic1, node no found"
+    exit 1
+  fi
+  NODE="`nvm which node`"
+  if [ ! -x "$NODE" ]; then
+    echo "!panic2, node no found."
+    exit 1
+  fi
+fi
+
+if [ 0 -ne `wine --version 2>&1 >/dev/null; echo $?` ]; then
+  sudo apt install wine-binfmt || echo 1
+fi
+
+if [ 0 -eq `npm install --quiet 2>&1 >/dev/null; echo $?` ]; then
+  $NODE boot.js $@
+else
+  echo "!panic, missing requirements."
+  exit 1
+fi
+
+# eof
